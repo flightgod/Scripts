189af4992046425e9db19edcad7eec32f2e4418e
Kevin Bennett <flightgod@gmail.com>
2017-04-10 16:00:14 -0500

    added compare folders script

---
+++ Compare-Folders.ps1
@@ -0,0 +1,5 @@
+﻿$LocalScripts = Get-ChildItem -Recurse -path C:\Scripts
+
+$remoteScripts = Get-ChildItem -Recurse -path H:\Scripts
+
+Compare-Object -ReferenceObject $LocalScripts -DifferenceObject $remoteScripts
