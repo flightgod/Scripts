ae033c5492badd2fe0e2f592fb9fd40c5ce0eeb0
Kevin Bennett <flightgod@gmail.com>
2017-05-01 14:09:22 -0500

    Updating Update-DTIContacts.ps1 Script-Ready for Run

--- Update-DTIContacts.ps1
+++ Update-DTIContacts.ps1
@@ -8,35 +8,47 @@
     that are no longer with DTI and flag for removal
 
 .NOTES  
-    Current Version     : 1.0
+    Current Version     	: 1.0
     
-    History				: 1.0 - Posted 5/1/2017 - First iteration - kbennett 
+    History			        : 1.0 - Posted 5/1/2017 - First iteration - kbennett 
         
-    Rights Required		: Mailbox Permissions
-                        : Requires PowerShell (or ISE) to 'Run as Administrator' to install the applications or modules
+    Rights Required		    : Exchange Permissions to Add/Edit Contacts
+				            : AD Permissions to Add/Edit Objects
+                        	: Requires PowerShell (or ISE) to 'Run as Administrator' to install the applications or modules
                         
-    Future Features     : Error Checking 
+    Future Features     	: Better Error Checking
+                            : Check and update additional Info from Export
 
 .FUNCTIONALITY
     Add Contacts, Update User Defined Field, List old Users
 #>
 
-# Connects to Exchange
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://ET016-EQEXMBX01.amer.epiqcorp.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $Session
 
 # Variables
-$import = Import-csv C:\Temp\List.csv
+$ExchangeServer = "http://ET016-EQEXMBX01.amer.epiqcorp.com/PowerShell/"
 $UDF = "MAY"
 $ContactOU = "OU=Contacts,OU=DTI,DC=amer,DC=EPIQCORP,DC=COM"
+$file = "C:\Temp\List.csv"
+$RemoveFile ="c:\Temp\RemoveList.csv"
+
+# Connects to Exchange
+Function ExchangeConnect 
+{
+    $UserCredential = Get-Credential
+    $Session = New-PSSession `
+    -ConfigurationName Microsoft.Exchange `
+    -ConnectionUri $ExchangeServer `
+    -Authentication Kerberos `
+    -Credential $UserCredential
+    Import-PSSession $Session
+}
 
 # Checks that Contact Exists
-Function CheckUser()
+Function CheckUser
 {
     foreach ($Name in $import)
     {
-        $CheckUser = Get-ADObject -LDAPFilter "objectClass=Contact" -SearchBase $ContactOU | Where {$_.Name -like $Name.DisplayName}
+        $CheckUser = Get-ADObject -LDAPFilter "objectClass=Contact" -SearchBase $ContactOU -Properties Name, Mail | ? {$_.Mail -like $Name.UserPrincipalName}
             If ($CheckUser -eq $Null)
             {
                 AddUser
@@ -48,24 +60,71 @@ Function CheckUser()
     }
 }
 
+# ********** Test this Seperatly **************
 # Updates User if it already exists
-Function UpdateUser()
+Function UpdateUser
 {
     Write-Host $Name.DisplayName "exists - Updating User Defined Field with" $UDF
+    Get-ADObject -LDAPFilter "objectClass=Contact" -SearchBase $ContactOU | `
+    ? {$_.Name -like $Name.DisplayName} | `
+    Set-ADObject -Add @{"extensionAttribute3"=$UDF} -WhatIf
 }
 
+# ********** Test this Seperatly **************
 # Adds new Contact if it doesnt already exist
-Function AddUser ()
+Function AddUser
 {
     Write-Host $Name.DisplayName "Doesnt Exist !!! - Creating Contact" -foregroundcolor red
-    $name
+    New-ADObject -name $name.DisplayName -type contact -Path $ContactOU `
+    -OtherAttributes @{`
+    'extensionAttribute3'=$UDF;`
+    'physicalDeliveryOfficeName'=$name.Office;`
+    'TelephoneNumber'=$name.BusinessPhone;`
+    'company'="DTI";`
+    'streetAddress'=$name.Address;`
+    'mobile'=$name.MobilePhone;`
+    'title'=$name.JobTitle;`
+    'department'=$name.Department;`
+    } -WhatIf
+    # also Mail-Enable
+    Enable-MailContact -Identity $Name.DisplayName -ExternalEmailAddress $name.UserPrincipalName -whatIf
+  
+}
+
+# Gets list of users that were not added this time
+Function ListUsersToDelete
+{
+    $RemoveList = Get-ADObject -LDAPFilter "objectClass=Contact" -SearchBase $ContactOU | `
+    ? {$_.extensionAttribute3 -NotLike $UDF -and $_.name -NotLike "DTI*"} | `
+    Select Name
+    Write-Host "The following users were not updated and added to list to remove:"
+    $RemoveList.Name
+    $testPath = Test-Path $RemoveFile
+    If ($testPath -eq $true)
+    {
+        Write-Warning "Ops, File is already there, I am removing"
+        Remove-Item $RemoveFile
+    }
+    $RemoveList.Name > $RemoveFile
+}
+
+# Checks that the file is there, then imports it
+Function ImportFile
+{
+$test = Test-Path $file
+    If ($test -eq $true)
+    {
+        $import = Import-csv $file
+    }
+    Else
+    {
+        Write-Warning "Something went Wrong: File is missing at $file"
+        Break
+    }
 }
 
 # Start Script
+ImportFile
+ExchangeConnect
 CheckUser
-
-
-
-
-# Exits Powershell Session with Exchange
-Exit-PSSession $Session
+ListUsersToDelete
