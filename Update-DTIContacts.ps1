0435be77f18a2c23a9a9214c3ea54c47261bf58c
Kevin Bennett <flightgod@gmail.com>
2017-05-03 13:50:53 -0500

    Finished DTIContact Script, added DL Update Script

--- Update-DTIContacts.ps1
+++ Update-DTIContacts.ps1
@@ -8,9 +8,10 @@
     that are no longer with DTI and flag for removal
 
 .NOTES  
-    Current Version     	: 1.0
+    Current Version     	: 1.1
     
     History			        : 1.0 - Posted 5/1/2017 - First iteration - kbennett 
+                            : 1.1 - Posted 5/2/2017 - Added better error checking - kbennett
         
     Rights Required		    : Exchange Permissions to Add/Edit Contacts
 				            : AD Permissions to Add/Edit Objects
@@ -64,35 +65,58 @@ Function CheckUser
 # Updates User if it already exists
 Function UpdateUser
 {
-    Write-Host $Name.DisplayName "exists - Updating User Defined Field with" $UDF
-    $UpdateUser = Get-ADObject -LDAPFilter "objectClass=Contact" -Server $DomainController -SearchBase $ContactOU | ? {$_.Name -like $Name.DisplayName}
-    # clear extensionAttribute3
-    Set-ADObject -Identity $UpdateUser -Clear "extensionAttribute3" -Server $DomainController -Credential $UserCredential
-    # Set extensionAttribute3
-    Set-ADObject -Identity $UpdateUser -Add @{"extensionAttribute3"=$UDF} -Server $DomainController -Credential $UserCredential
+    $UpdateUser = Get-ADObject -LDAPFilter "objectClass=Contact" -Server $DomainController -SearchBase $ContactOU -Properties DisplayName, extensionAttribute3 | ? {$_.Name -like $Name.DisplayName}
+    # Checking if Value extensionAttribute3 is already present and correct
+    if ($UpdateUser.extensionAttribute3 -ne $UDF)
+    { 
+        # clear extensionAttribute3
+        Write-Host $Name.DisplayName "exists - Updating User Defined Field with" $UDF
+        Set-ADObject -Identity $name.DisplayName -Clear "extensionAttribute3" -Server $DomainController -Credential $UserCredential 2>> c:\temp\UpdateErrors.txt
+        # Set extensionAttribute3
+        Set-ADObject -Identity $name.DisplayName -Add @{"extensionAttribute3"=$UDF} -Server $DomainController -Credential $UserCredential
+    }
+    Else 
+    { 
+        Write-Host $Name.DisplayName "Exists - With correct extensionAttribute3 Value" $UDF
+    }
 }
 
 # Adds new Contact if it doesnt already exist
 Function AddUser 
 {
+    # Error Checking incase the Source file has blanks
+    # sets variables to something
+    $Title = "Title"
+    $Office = "Office"
+    $TelephoneNumber = "Phone"
+    $streetAddress = "Street"
+    $Mobile = "Mobile"
+    $department = "Dept"
     $NewName = $Name.DisplayName
     Write-Host $NewName "Doesnt Exist !!! - Creating Contact" -foregroundcolor Yellow
-    New-ADObject -name $NewName -type contact -Path $ContactOU -Server $DomainController -Credential $UserCredential -OtherAttributes @{'extensionAttribute3'=$UDF;'physicalDeliveryOfficeName'=$name.Office;'TelephoneNumber'=$name.BusinessPhone;'company'="DTI";'streetAddress'=$name.Address;'mobile'=$name.MobilePhone;'title'=$name.JobTitle;'department'=$name.Department}
+    # adds proper values to variables if they are present
+    If ($name.Office) {$Office = $name.Office}
+    If ($name.BusinessPhone) {$TelephoneNumber = $name.BusinessPhone}
+    If ($name.Address) {$streetAddress = $name.Address}
+    If ($name.MobilePhone) {$Mobile = $name.MobilePhone}
+    If ($name.JobTitle) {$Title = $name.JobTitle}
+    If ($name.department) {$department = $name.department}
+    # Adds new object
+    New-ADObject -name $NewName -type contact -Path $ContactOU -Server $DomainController -Credential $UserCredential -OtherAttributes @{'extensionAttribute3'=$UDF;'physicalDeliveryOfficeName'=$Office;'TelephoneNumber'=$TelephoneNumber;'company'="DTI";'streetAddress'=$streetAddress;'mobile'=$Mobile;'title'=$Title;'department'=$department} 2>> c:\temp\AddErrors.txt
     # also Mail-Enable
-    Enable-MailContact -Identity $Name.DisplayName -ExternalEmailAddress $name.UserPrincipalName -DomainController $DomainController
+    Enable-MailContact -Identity $Name.DisplayName -ExternalEmailAddress $name.UserPrincipalName -DomainController $DomainController 2>> c:\temp\EnableErrors.txt
+    # create me an array of users that were added
     $NewContactsList = @()
-    
     $script:NewContactsList += $Newname
    }
 
 # Gets list of users that were not added this time
 Function ListUsersToDelete
 {
-    $RemoveList = Get-ADObject -LDAPFilter "objectClass=Contact" -Server $DomainController -SearchBase $ContactOU -Properties Name, Mail | `
-    ? {$_.extensionAttribute3 -NotLike $UDF -or $_.name -NotLike "DTI*"} | `
+    $RemoveList = Get-ADObject -LDAPFilter "objectClass=Contact" -Server $DomainController -SearchBase $ContactOU -Properties Name, Mail, extensionAttribute3 | `
+    ? {$_.extensionAttribute3 -NotLike $UDF} | `
     Select Name, Mail
-    Write-Host "The following users were not updated and added to list to remove:"
-    #$RemoveList
+    Write-Host "Number of users that were not updated this time:"$RemoveList.count
     $testPath = Test-Path $RemoveFile
         If ($testPath -eq $true)
         {
---
+++ Update-DistroList.ps1
@@ -0,0 +1,82 @@
+﻿<#  
+.SYNOPSIS
+   	Update Epiq All Distribution List
+
+.DESCRIPTION  
+    This script will read list of FTE Users from Workday, verify that the user exists in AD, and see if it is 
+    already a member of the Epiq All DL. If not it will add it
+
+.NOTES  
+    Current Version     	: 1.0
+    
+    History			        : 1.0 - Posted 5/3/2017 - First iteration - kbennett 
+        
+    Rights Required		    : AD Permissions to Add/Edit Objects
+                        	: Requires PowerShell (or ISE) to 'Run as Administrator' to install the applications or modules
+                        
+    Future Features     	: Better Error Checking
+             
+.FUNCTIONALITY
+    Update Distibution List, Check user exists
+#>
+
+# Variables
+$ImportFile = "C:\Temp\FTEList.csv"
+$DomainController = "P016ADSAMDC01.amer.EPIQCORP.COM"
+$GroupName = "Epiq-All@Epiqsystems.com"
+$UserCredential = Get-Credential
+
+# Function import list of users
+Function ImportList 
+{
+    $test = Test-Path $ImportFile
+    If ($test -eq $true)
+    {
+        $script:import = Import-csv $ImportFile
+    }
+    Else
+    {
+        Write-Warning "Something went Wrong: File is missing at $ImportFile"
+        Break
+    }
+}
+
+# Function Check user exists
+Function CheckUser
+{
+    foreach ($Name in $import)
+    {
+        $script:UserInfo = $Name."Email Address"
+        # Only checks if Email address is epiqsystems.com
+        If ($UserInfo -Like "*epiqsystems.com")
+        {
+        $script:CheckUser = Get-ADuser -Filter "EmailAddress -like '$UserInfo'" -Properties Name,EmailAddress -Server $DomainController
+            If ($CheckUser -eq $Null)
+            { Write-host $Name.Worker " Doesn't Exist in AD - Skipping" -foregroundcolor Red 2>> c:\temp\UserDoesntExist.csv} 
+            Else 
+            { CheckDL ($UserCredential) }
+        }
+    }
+}
+
+# Function Check User in DL
+Function CheckDL
+{
+    $members = Get-ADGroupMember -Identity $GroupName -Recursive | Select -ExpandProperty Name
+    If ($members -contains $CheckUser.Name) {Write-Host $CheckUser.EmailAddress "exists in the group"} 
+    Else {
+    # AddUser ($UserCredential)
+    Write-Host $CheckUser.EmailAddress "Being Added to group" $GroupName -foregroundcolor Yellow
+    AddUser ($UserCredential)
+    } 
+}
+
+# Function Add new user to DL
+Function AddUser
+{
+    Add-ADGroupMember -Identity $GroupName -Members $CheckUser.SamAccountName -Credential $UserCredential
+}
+
+# Begin of Script
+ImportList ($ImportFile)
+CheckUser ($UserCredential)
