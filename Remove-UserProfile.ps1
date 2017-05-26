9054acabc35e88d4947a2266adbe321989a653d0
Kevin Bennett <flightgod@gmail.com>
2017-05-26 15:43:37 -0500

    updates

---
+++ Remove-UserProfile.ps1
@@ -0,0 +1,27 @@
+﻿<#  
+.SYNOPSIS
+   REmove Old User Profiles
+
+.DESCRIPTION  
+    This script will remove old User Profiles
+
+.NOTES  
+    Current Version     : 1.0
+    
+    History				: 1.0 - Posted 5/26/2017 - First iteration - kbennett                      
+    
+    Rights Required		: Requires PowerShell (or ISE) to 'Run as Administrator' to install the applications or modules
+                        
+    Future Features     : Error Checking 
+
+.FUNCTIONALITY
+    Search Last used, Delete Folder
+#>
+
+
+
+Get-CimInstance -ClassName Win32_UserProfile | 
+Where {(!$_.Special) -and ($_.LastUseTime -lt (Get-Date).AddDays(-180))} |
+ForEach-Object {
+$_ | Remove-CimInstance
+}
--- Update-Password.ps1
+++ Update-Password.ps1
@@ -1,6 +1,29 @@
-﻿#Update password
+﻿<#  
+.SYNOPSIS
+   Update Password
 
-$NewPassword = ""
+.DESCRIPTION  
+    This script will change a users password on Epiqcorp and xxCust Environments
+
+.NOTES  
+    Current Version     : 1.0
+    
+    History				: 1.0 - Posted 5/26/2017 - First iteration - kbennett                      
+    
+    Rights Required		: Domain Permissions in each domain
+                        : Requires PowerShell (or ISE) to 'Run as Administrator' to install the applications or modules
+                        
+    Future Features     : Error Checking 
+                        : Add More Domains
+                        : Maybe make it a wildcard search so you can pick .. or not?
+
+.FUNCTIONALITY
+    Change Password
+#>
+
+# Variables
+$AccountName = Read-Host "Enter UserName to change password"
+$NewPassword = Read-Host "Enter the new Password"
 $Servers = @(`
     "P016ADSACDC01.apcust.local",`
     "P016ADSCCDC01.cacust.local",`
@@ -8,30 +31,36 @@ $Servers = @(`
     "P016ADSECDC01.eucust.local",`
     "P016ADSUCDC01.uscust.local",`
     "p016adsamdc02.amer.epiqcorp.com",`
-    "amer.epiqcorp.com",`
+    "epiqcorp.com",`
     "apac.epiqcorp.com",`
     "euro.epiqcorp.com",`
-    "epiqcorp.com")
+    "amer.epiqcorp.com")
 
-If ($UserName -eq $null){
-    $UserCredentials = Get-Credential
-    }
-
-$UserName = Read-Host "Enter UserName to change password"
-$NewPassword = Read-Host "Enter the new Password"
-Foreach ($DC in $Servers){
-    try {
-        Set-ADAccountPassword $UserName `
-            -Reset `
-            -NewPassword (ConvertTo-SecureString -AsPlainText $NewPassword -Force) `
-            -Server $DC `
-            -Credential $UserCredentials `
-            -whatif
-        Write-Host "RESET password for" $UserName "on $DC"-ForegroundColor Green
-    }
-    catch {
-        $ErrorMessage = $_.Exception.Message
-        $FailedItem = $_.Exception.ItemName
-        Write-Host $ErrorMessage -ForegroundColor Red
+Function GetCreds {
+    If ($UserCredentials.UserName -eq $null){
+        $UserCredentials = Get-Credential
     }
 }
+
+Function ChangePassword {
+    Foreach ($DC in $Servers){
+        try {
+            Set-ADAccountPassword $AccountName `
+                -Reset `
+                -NewPassword (ConvertTo-SecureString -AsPlainText $NewPassword -Force) `
+                -Server $DC `
+                -Credential $UserCredentials
+            Write-Host "RESET password for" $AccountName "on $DC"-ForegroundColor Green
+        }
+        catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            Write-Host $ErrorMessage -ForegroundColor Red
+        }
+    }
+}
+
+# Main Script
+GetCreds
+ChangePassword
+
