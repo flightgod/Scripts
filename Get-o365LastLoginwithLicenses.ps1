058c83e9ae082ea2e05c68797b254bf1043895a7
Kevin Bennett <flightgod@gmail.com>
2018-06-07 10:00:04 -0500

    Updating Changes

---
+++ Get-o365LastLoginwithLicenses.ps1
@@ -0,0 +1,30 @@
+﻿Function Connect-o365 {
+    $O365URI = "https://outlook.office365.com/powershell-liveid/"
+    If ($Session.ComputerName -like "outlook.office365.com") {
+        Write-Host "Session already established to exchange" -ForegroundColor Green
+    }
+    Else {
+        Write-Host "Session not made to exchange, creating session now" -ForegroundColor Red
+        $script:o365Credential = Get-Credential
+        Import-Module MSOnline
+        Connect-MsolService -Credential $o365Credential
+        $o365Session = New-PSSession `
+        -ConfigurationName Microsoft.Exchange `
+        -ConnectionUri $O365URI `
+        -Authentication Basic `
+        -AllowRedirection `
+        -Credential $o365Credential
+        Import-PSSession $o365Session
+    }
+}
+
+connect-o365
+
+Get-MsolUser -All |
+  Where {$_.IsLicensed -eq $true } |
+    Foreach {Get-MailboxStatistics $_.UserPrincipalName |
+    Select DisplayName,UsageLocation,@{n="Licenses Type";e={$_.Licenses.AccountSKUid}},Email,LastLogonTime} | 
+  Export-Csv -Path C:\Temp\Test.csv -NoTypeInformation
+
+
+  Get-ADUser -filter * | Where {$_.pwdLastSet -eq $NULL} | Select SamAccountName, UserPrincipalName, pwdLastSet, whenCreated
