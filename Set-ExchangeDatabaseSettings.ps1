5d3f8f0200214fdc261fbf0f6bbde3b923584bf8
Kevin Bennett <flightgod@gmail.com>
2020-06-10 08:23:41 -0500

    normalized

--- Set-ExchangeDatabaseSettings.ps1
+++ Set-ExchangeDatabaseSettings.ps1
@@ -27,7 +27,7 @@
 
 # Connects to Exchange
 $UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://ET016-EQEXMBX01.amer.epiqcorp.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server.amer.domain.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
 Import-PSSession $Session
 
 # Variables
@@ -35,7 +35,7 @@ $Database = Read-Host -Prompt 'Input the Database name'
 
 Set-MailboxDatabase $Database -RecoverableItemsQuota 100GB
 Set-MailboxDatabase $Database -RecoverableItemsWarningQuota 80GB
-Set-MailboxDatabase $Database -RpcClientAccessServer ET016-EX10HUB1.amer.EPIQCORP.COM
+Set-MailboxDatabase $Database -RpcClientAccessServer HUB1.amer.domain.COM
 Set-MailboxDatabase $Database -MaintenanceSchedule {Tue.1:00 AM-Tue.5:00 AM, Wed.1:00 AM-Wed.5:00 AM, Thu.1:00 AM-Thu.5:00 AM, Fri.1:00 AM-Fri.5:00 AM, Fri.6:00 PM-Mon.5:00 AM}
 Set-MailboxDatabase $Database -DeletedItemRetention 29.00:00:00
 Set-MailboxDatabase $Database -OfflineAddressBook '\Offline Address Book'
