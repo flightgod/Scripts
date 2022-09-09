6a0c46ce1474dc4a6253b5d46e7ecd727bd421aa
Kevin Bennett <flightgod@gmail.com>
2022-09-09 13:31:40 -0500

    Add files via upload

---
+++ Get-DomainWhoIsInfo.ps1
@@ -0,0 +1,71 @@
+﻿<#  
+.SYNOPSIS
+   	Runs to get Domain Whois Info From Whoisxmlapi
+.DESCRIPTION  
+    Provides exports of various Domain Whois Info
+.USAGE
+   .\Get-DomainWhoISInfo.ps1 -APIKey <APIKey> -DomainName <DomainName>
+
+.NOTES  
+    Current Version     		: 1.0
+    
+    History			        : 1.1 - Posted 6/27/2022 - Creating Script - kbennett 
+
+         
+    Rights Required		    	: Global Reader for Azure Tenant
+                        		: Requires PowerShell (or ISE) to 'Run as Administrator' to install the applications or modules
+                        
+    Future Features     	
+                            		: Check connection is done
+                            		: Error Checking
+
+             
+.FUNCTIONALITY
+    Get Whois Details and export CSV files
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $APIKey, 
+    [Parameter(Mandatory = $true)]
+    [string[]]
+    $DomainName
+)
+
+$responses = @()
+$DomainName | ForEach-Object {
+    $requestUri = "https://www.whoisxmlapi.com/whoisserver/WhoisService?"`
+    + "apiKey=$apiKey"`
+    + "&domainName=$domainName"`
+    + "&outputFormat=JSON"
+    $responses += Invoke-RestMethod -Method Get -Uri $requestUri
+}
+
+function Get-ValidDate ($Value, $Date) {
+    $defaultDate = $Value."$($Date)Date"
+    $normalizedDate = $Value.registryData."$($Date)DateNormalized"
+
+    if (![string]::IsNullOrEmpty($defaultDate)) {
+        return Get-Date $defaultDate
+    }
+
+    return [datetime]::ParseExact($normalizedDate, "yyyy-MM-dd HH:mm:ss UTC", $null)    
+}
+
+$properties = "domainName", "domainNameExt",
+@{N = "createdDate"; E = { Get-ValidDate $_ "created" } },
+@{N = "updatedDate"; E = { Get-ValidDate $_ "updated" } },
+@{N = "expiresDate"; E = { Get-ValidDate $_ "expires" } },
+"registrarName",
+"contactEmail",
+"estimatedDomainAge",
+# @{N = "registrant"; e = { $_.registrant.rawtext } },
+@{N = "contact"; e = { ($_.registrant | Select-Object -Property * -ExcludeProperty rawText ).PSObject.Properties.Value -join ", " } }
+
+$whoIsInfo = $responses.WhoisRecord | Select-Object -Property $properties
+
+$whoIsInfo | Export-Csv -NoTypeInformation d:\temp\domain-whois.csv
+
+$whoIsInfo | Format-Table
---
+++ port-scan-tcp.ps1
@@ -0,0 +1,51 @@
+﻿Function port-scan-tcp {
+  param($hosts,$ports)
+  if (!$ports) {
+    Write-Host "usage: port-scan-tcp <host|hosts> <port|ports>"
+    Write-Host " e.g.: port-scan-tcp 192.168.1.2 445`n"
+    return
+  }
+  $out = ".\scanresults.txt"
+  foreach($p in [array]$ports) {
+   foreach($h in [array]$hosts) {
+    $x = (gc $out -EA SilentlyContinue | select-string "^$h,tcp,$p,")
+    if ($x) {
+      gc $out | select-string "^$h,tcp,$p,"
+      continue
+    }
+    $msg = "$h,tcp,$p,"
+    $t = new-Object system.Net.Sockets.TcpClient
+    $c = $t.ConnectAsync($h,$p)
+    for($i=0; $i -lt 10; $i++) {
+      if ($c.isCompleted) { break; }
+      sleep -milliseconds 1000
+    }
+    $t.Close();
+    $r = "Filtered"
+    if ($c.isFaulted -and $c.Exception -match "actively refused") {
+      $r = "Closed"
+    } elseif ($c.Status -eq "RanToCompletion") {
+      $r = "Open"
+    }
+    $msg += $r
+    Write-Host "$msg"
+    echo $msg >>$out
+   }
+  }
+}
+
+
+
+# port-scan-tcp (gc c:\temp\ip.txt) (21,22,23,25,53,80,110,111,135,138,139,143,443,445,993,995,1723,3306,3389,5900,8080)
+# port-scan-tcp (gc c:\temp\ip.txt) (21,23,25)
+
+# .NET 4.5
+
+# Examples:
+#
+# port-scan-tcp 10.10.0.1 137
+# port-scan-tcp 10.10.0.1 (135,137,445)
+# port-scan-tcp (gc .\ips.txt) 137
+# port-scan-tcp (gc c:\temp\ip.txt) (21,22,23,25,53,80,110,111,135,138,139,143,443,445,993,995,1723,3306,3389,5900,8080)
+# 0..255 | foreach { port-scan-tcp 10.10.0.$_ 137 }
+# 0..255 | foreach { port-scan-tcp 10.10.0.$_ (135,137,445) }
