c7e1b3a21a43b35ea536cac143b2406b9ac7f579
Kevin Bennett <flightgod@gmail.com>
2022-09-09 13:31:49 -0500

    Add files via upload

---
+++ Git-Pull.ps1
@@ -0,0 +1,13 @@
+﻿#change your github location
+$git_dir = "c:\temp"
+
+
+Get-ChildItem  $git_dir | ForEach-Object {
+    if($_.Attributes -eq "Directory")
+    {
+      Write-Host $_.FullName
+      Set-Location $_.FullName
+      git fetch
+      git pull
+    }
+  }
